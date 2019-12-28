103d334f0c8756ee6ff86daadc89915642a6c28b
645874679 <645874679@qq.com>
2019-12-28 16:06:24 +0800

    init

--- src/com/artcweb/constant/SiteConstant.java
+++ src/com/artcweb/constant/SiteConstant.java
@@ -3,7 +3,7 @@ package com.artcweb.constant;
 
 public class SiteConstant {
 	
-	public static final String HTTP_DOMAIN="http://127.0.0.1:8055/";
-	//public static final String HTTP_DOMAIN="https://dz.artcweb.com/";
+	//public static final String HTTP_DOMAIN="http://127.0.0.1:8055/";
+	public static final String HTTP_DOMAIN="https://dz.artcweb.com/";
 
 }
